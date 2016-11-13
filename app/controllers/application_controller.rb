ebee8c7fd89f506365313f6a6fa46cf49ac6285a
Thinh Nguyen <nguyendthinh@outlook.com>
2016-11-13 12:51:21 -0500

    implemented user sign-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :list_brands
+  before_action :list_brands, :set_current_user
 
   def list_brands
     @brands = Brand.all
   end
+
+  def set_current_user
+    if User.exists?(session[:user_id])
+      @current_user = User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
 
   resources :brands
   resources :sneakers
+  resources :users
+  resource :session
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
