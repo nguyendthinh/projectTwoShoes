f3adc3ed0c32ae84030de20db003edaa6fae0680
Thinh Nguyen <nguyendthinh@outlook.com>
2016-11-16 17:12:29 -0500

    cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-before_action :set_session
+before_action :set_session, :total
 
   def set_session
     session[:order] ||= []
@@ -19,6 +19,17 @@ before_action :set_session
     session[:order] = []
     @order = session[:order]
     redirect_to orders_path
+  end
+
+  def total
+    @total
+    session[:order].each do |sneaker|
+      @allPrices = sneaker["price"]
+      puts "*" * 50
+      puts @allPrices
+      puts "*" * 50
+      @total + @allPrices = @total
+    end
   end
 
   def destroy
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,15 @@
 class Order < ApplicationRecord
   has_many :sneakers
   belongs_to :user, optional: true
+  before_action :total
+
+  def total
+    session[:order].each do |sneaker|
+      @allPrices = sneaker["price"].to_i
+      puts @allPrices
+      @total += @allPrices
+    end
+    @total
+  end
+
 end
